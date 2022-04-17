cab235f9c4b4ea8d8979472f7da7bee9e0c82f26
Edgar <edgargc.upc@gmail.com>
2022-04-17 20:05:51 +0200

    problem definition in typescript

---
+++ TypeScript/jest.config.ts
@@ -0,0 +1,15 @@
+import { pathsToModuleNameMapper } from "ts-jest/utils";
+const { compilerOptions } = require("./tsconfig");
+
+export default {
+  roots: ["<rootDir>/app", "<rootDir>/test/jest"],
+  collectCoverage: true,
+  coverageDirectory: "coverage",
+  coverageProvider: "v8",
+  transform: {
+    "^.+\\.tsx?$": "ts-jest"
+  },
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+    prefix: "<rootDir>/"
+  })
+};
